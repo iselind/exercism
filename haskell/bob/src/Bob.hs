748270b65a0f5c40a91e252c19941989b7a14def
Patrik Iselind <patrik.mrx@gmail.com>
2018-12-05 19:39:31 +0100

    Add some move coverage

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -5,6 +5,8 @@ import Data.Char
 responseFor :: String -> String
 responseFor xs
     | '.' == (last xs) = "Whatever."
+    | (map toUpper xs) == xs && '?' == (last xs) = "Calm down, I know what I'm doing!"
+    | (map toUpper xs) == (map toLower xs) = "Whatever."
     | (map toUpper xs) == xs = "Whoa, chill out!"
     | '?' == (last xs) = "Sure."
     | otherwise = "Whatever."
