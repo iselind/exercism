38a3b8ed074b5649c7b71b24a285b7e72ce03663
Patrik Iselind <patrik.mrx@gmail.com>
2018-12-09 20:52:33 +0100

    Made some refactoring of the solution

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -2,11 +2,15 @@ module Bob (responseFor) where
 
 import Data.Char
 
+isQuestion :: String -> Bool
+isQuestion xs = '?' == (last xs)
+
+isYelling :: String -> Bool
+isYelling xs = ((map toUpper xs) == xs) && ((map toLower xs) /= xs )
+
 responseFor :: String -> String
 responseFor xs
-    | '.' == (last xs) = "Whatever."
-    | (map toUpper xs) == xs && '?' == (last xs) = "Calm down, I know what I'm doing!"
-    | (map toUpper xs) == (map toLower xs) = "Whatever."
-    | (map toUpper xs) == xs = "Whoa, chill out!"
-    | '?' == (last xs) = "Sure."
+    | (isYelling xs) && (isQuestion xs) = "Calm down, I know what I'm doing!"
+    | (isQuestion xs) = "Sure."
+    | (isYelling xs) = "Whoa, chill out!"
     | otherwise = "Whatever."
