1b907bf297d13e542a73bca754d905ab6c05696e
Patrik Iselind <patrik.mrx@gmail.com>
2018-12-09 21:05:01 +0100

    Detect some jibberish

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -8,8 +8,12 @@ isQuestion xs = (not $ null xs) && ('?' == (last xs))
 isYelling :: String -> Bool
 isYelling xs = ((map toUpper xs) == xs) && ((map toLower xs) /= xs )
 
+isJibberish :: String -> Bool
+isJibberish xs = null xs
+
 responseFor :: String -> String
 responseFor xs
+    | (isJibberish xs) = "Fine. Be that way!"
     | (isYelling xs) && (isQuestion xs) = "Calm down, I know what I'm doing!"
     | (isQuestion xs) = "Sure."
     | (isYelling xs) = "Whoa, chill out!"
