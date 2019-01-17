e9db11d3cf09d73d7c839aa8926e98e3c25150bb
Patrik Iselind <patrik.mrx@gmail.com>
2019-01-17 20:57:12 +0100

    Take Either into account

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -5,13 +5,13 @@ import qualified Data.Map as Map
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
-count :: String -> Map Nucleotide Int-> Map Nucleotide Int
-count [] result = result
+count :: String -> Map Nucleotide Int-> Either String (Map Nucleotide Int)
+count [] result = Right result
 count ('A':xs) result = count xs (Map.insert A ((result ! A)+1) result)
 count ('C':xs) result = count xs (Map.insert C ((result ! C)+1) result)
 count ('G':xs) result = count xs (Map.insert G ((result ! G)+1) result)
 count ('T':xs) result = count xs (Map.insert T ((result ! T)+1) result)
-count (_:xs) result = count xs result
+count (_:xs) result = Left "Bad character in input"
 
-nucleotideCounts :: String -> Map Nucleotide Int
+nucleotideCounts :: String -> Either String (Map Nucleotide Int)
 nucleotideCounts xs = count xs (Map.fromList([(A,0),(C,0),(G,0),(T,0)]))
