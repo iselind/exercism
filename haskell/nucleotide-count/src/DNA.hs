cc823a3d171806c3c64ec1c34c2cbdd3f18eb587
Patrik Iselind <patrik.mrx@gmail.com>
2019-01-17 20:45:49 +0100

    We have something working while ignoring Either

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,8 +1,17 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
-import Data.Map (Map)
+import Data.Map (Map,(!))
+import qualified Data.Map as Map
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
-nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+count :: String -> Map Nucleotide Int-> Map Nucleotide Int
+count [] result = result
+count ('A':xs) result = count xs (Map.insert A ((result ! A)+1) result)
+count ('C':xs) result = count xs (Map.insert C ((result ! C)+1) result)
+count ('G':xs) result = count xs (Map.insert G ((result ! G)+1) result)
+count ('T':xs) result = count xs (Map.insert T ((result ! T)+1) result)
+count (_:xs) result = count xs result
+
+nucleotideCounts :: String -> Map Nucleotide Int
+nucleotideCounts xs = count xs (Map.fromList([(A,0),(C,0),(G,0),(T,0)]))
