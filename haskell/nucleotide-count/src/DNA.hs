b97af49f49a7fe2708c4a8c703c1cdb7fd2ac749
Patrik Iselind <patrik.mrx@gmail.com>
2019-01-19 19:51:18 +0100

    Use foldl

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -5,15 +5,16 @@ import qualified Data.Map.Strict as Map
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
-isNucleoticeString :: String -> Bool
-isNucleoticeString [] =  True
-isNucleoticeString (x:xs)
-    | x == 'A' = isNucleoticeString xs
-    | x == 'C' = isNucleoticeString xs
-    | x == 'G' = isNucleoticeString xs
-    | x == 'T' = isNucleoticeString xs
+-- Check if a string characters correspond to a Nucleotide
+isNucleoticeStringCharacter :: Char -> Bool
+isNucleoticeStringCharacter x
+    | x == 'A' = True
+    | x == 'C' = True
+    | x == 'G' = True
+    | x == 'T' = True
     | otherwise = False
 
+-- Convert a string of characters to a list of Nucleotides
 stringToNucleotides :: String -> [Nucleotide]
 stringToNucleotides [] = []
 stringToNucleotides (x:xs)
@@ -22,12 +23,13 @@ stringToNucleotides (x:xs)
     | x == 'G' = G : stringToNucleotides xs
     | x == 'T' = T : stringToNucleotides xs
 
-count :: [Nucleotide] -> Map Nucleotide Int-> Map Nucleotide Int
-count [] result = result
-count (x:xs) result = count xs (Map.insertWith (+) x 1 result)
+-- Given a Map and a Nucleotide, update the Map accordingly
+count :: Map Nucleotide Int -> Nucleotide -> Map Nucleotide Int
+count result x = Map.insertWith (+) x 1 result
 
+-- Either count the Nucleotides in first parameter, or return error
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
 nucleotideCounts xs
-    | isNucleoticeString xs == False = Left "Bad character in input"
+    | not (all isNucleoticeStringCharacter xs) = Left "Bad character in input"
     | otherwise = Right answer
-    where answer = count (stringToNucleotides xs) (Map.fromList([(A,0),(C,0),(G,0),(T,0)]))
+    where answer = foldl count (Map.fromList([(A,0),(C,0),(G,0),(T,0)])) (stringToNucleotides xs)
