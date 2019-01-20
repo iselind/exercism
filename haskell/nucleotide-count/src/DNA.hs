c27db4a03bd799902866f14e9b6bd5924c28ebc0
Patrik Iselind <patrik.mrx@gmail.com>
2019-01-20 07:44:19 +0100

    Remove repetition in isNucleoticeStringCharacter

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,17 +1,18 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
+-- Import the type Map from Data.Map.Strict
 import Data.Map.Strict (Map)
+-- Specify that to use any of the Map operations we need to qualify them
 import qualified Data.Map.Strict as Map
+
+-- Specify that to use any of the List operations we need to qualify them
+import qualified Data.List as List
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 -- Check if a string characters correspond to a Nucleotide
 isNucleoticeStringCharacter :: Char -> Bool
-isNucleoticeStringCharacter 'A' = True
-isNucleoticeStringCharacter 'C' = True
-isNucleoticeStringCharacter 'G' = True
-isNucleoticeStringCharacter 'T' = True
-isNucleoticeStringCharacter _ = False
+isNucleoticeStringCharacter x = x `List.elem` "ACGT"
 
 -- Convert a string of characters to a list of Nucleotides
 stringToNucleotides :: String -> [Nucleotide]
