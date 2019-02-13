97773dbcfbbc079106cd4e12f38fb6cc304f3819
Patrik Iselind <patrik.mrx@gmail.com>
2019-02-13 22:40:12 +0100

    Some of it starts to work

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -1,15 +1,23 @@
 module School (School, add, empty, grade, sorted) where
 
-data School = Dummy
+-- A school is a list of grades, in each grade we have a list of
+-- students
+data School = Grade Int [String] School | Empty deriving (Show)
 
 add :: Int -> String -> School -> School
-add gradeNum student school = error "You need to implement this function."
+add gradeNum student Empty = Grade gradeNum (student : []) Empty
+add gradeNum student (Grade num members school)
+    | gradeNum == num = Grade gradeNum (student : members) school
+    | otherwise = Grade num members (add gradeNum student school)
 
 empty :: School
-empty = error "You need to implement this function."
+empty = Empty
 
 grade :: Int -> School -> [String]
-grade gradeNum school = error "You need to implement this function."
+grade _ Empty = []
+grade gradeNum (Grade num members school)
+    | gradeNum == num = members
+    | otherwise = grade gradeNum school
 
 sorted :: School -> [(Int, [String])]
 sorted school = error "You need to implement this function."
