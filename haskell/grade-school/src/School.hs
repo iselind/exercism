b355d005a046be500a53cc9e07134606b07698d2
Patrik Iselind <patrik.mrx@gmail.com>
2019-02-17 19:18:58 +0100

    Sort the members

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -3,14 +3,16 @@ module School (School, add, empty, grade, sorted) where
 import Data.List (sortBy)
 
 -- A school is a list of grades, in each grade we have a list of
--- students
+-- students. A list is a recursive data structure, so all functions working
+-- with Schools are expressed in a recursive manner.
 data School = Grade Int [String] School | Empty deriving (Show)
 
 add :: Int -> String -> School -> School
 add gradeNum student Empty = Grade gradeNum (student : []) Empty
 add gradeNum student (Grade num members school)
-    | gradeNum == num = Grade gradeNum (student : members) school
+    | gradeNum == num = Grade gradeNum sortedStudents school
     | otherwise = Grade num members (add gradeNum student school)
+    where sortedStudents = sortBy (\a b -> compare a b) (student : members)
 
 empty :: School
 empty = Empty
