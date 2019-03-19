9e98589d8cf42eeeb3c3d65806382dae845882da
Patrik Iselind <patrik.mrx@gmail.com>
2019-03-19 19:58:29 +0100

    And a similar short version but we use the .(dot) operator

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -5,8 +5,10 @@ next x
     | (even x) = x `quot` 2
     | otherwise = 3*x+1
 
+collatz' :: Integer -> Integer
+collatz' = toInteger . length . (takeWhile (/= 1)) . (iterate next)
+
 collatz :: Integer -> Maybe Integer
 collatz x
     | (x < 1) = Nothing
-    | otherwise = Just (toInteger answer)
-    where answer = length $ takeWhile (/= 1) $ (iterate next x)
+    | otherwise = Just (collatz' x)
