886a5a2eaf5f391fb7a2ae746dffa926d1c6d36e
Patrik Iselind <patrik.mrx@gmail.com>
2019-03-19 19:46:48 +0100

    Simplified using $

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,18 +1,12 @@
 module CollatzConjecture (collatz) where
 
--- The only case where Nothing is returned is for zero and negative numbers.
--- Otherwise we always get a sane value. I think it would complicate the
--- calculations to include the Maybe type in it, so I split collatz in two. One
--- part takes care of checking if inparameter is more than zero, and the other
--- part takes care of the actual calculations.
-
-collatz' :: Integer -> Integer
-collatz' 1 = 0
-collatz' x
-    | (x `mod` 2 == 0) = (1 + (collatz' (x `div` 2)))
-    | otherwise = (1 + (collatz' (3*x+1)))
+next :: Integer -> Integer
+next x
+    | (even x) = x `quot` 2
+    | otherwise = 3*x+1
 
 collatz :: Integer -> Maybe Integer
 collatz x
     | (x < 1) = Nothing
-    | otherwise = Just (collatz' x)
+    | otherwise = Just (toInteger answer)
+    where answer = length $ takeWhile (/= 1) $ (iterate next x)
