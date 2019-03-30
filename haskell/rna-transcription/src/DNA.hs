853ebc9ab172963690563784d6a451f64cd646fe
Patrik Iselind <patrik.mrx@gmail.com>
2019-03-30 19:13:38 +0100

    First attempt

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,4 +1,17 @@
 module DNA (toRNA) where
 
+rnaTransform :: Char -> Char
+rnaTransform 'C' = 'G'
+rnaTransform 'G' = 'C'
+rnaTransform 'A' = 'U'
+rnaTransform 'T' = 'A'
+rnaTransform _ = error "BOOM"
+
+dnaNucleotides :: String
+dnaNucleotides = "CGAT"
+
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA "" = Right ""
+toRNA xs
+    | all (`elem` dnaNucleotides) xs = Right (map rnaTransform xs)
+    | otherwise = Left (head $ filter (`notElem` dnaNucleotides) xs)
