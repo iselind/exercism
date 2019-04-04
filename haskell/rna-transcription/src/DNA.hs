e549fdf6b093c917aecb418fa627fd9cea4b8341
Patrik Iselind <patriki@axis.com>
2019-04-04 16:39:45 +0200

    Simplify
    Change-Id: I360d216b5b4591cff3997ec523db45318b4653c6

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,17 +1,11 @@
 module DNA (toRNA) where
 
-rnaTransform :: Char -> Char
-rnaTransform 'C' = 'G'
-rnaTransform 'G' = 'C'
-rnaTransform 'A' = 'U'
-rnaTransform 'T' = 'A'
-rnaTransform _ = error "BOOM"
-
-dnaNucleotides :: String
-dnaNucleotides = "CGAT"
+rnaTransform :: Char -> Either Char Char
+rnaTransform 'C' = Right 'G'
+rnaTransform 'G' = Right 'C'
+rnaTransform 'A' = Right 'U'
+rnaTransform 'T' = Right 'A'
+rnaTransform x =  Left x
 
 toRNA :: String -> Either Char String
-toRNA "" = Right ""
-toRNA xs
-    | all (`elem` dnaNucleotides) xs = Right (map rnaTransform xs)
-    | otherwise = Left (head $ filter (`notElem` dnaNucleotides) xs)
+toRNA = traverse rnaTransform
