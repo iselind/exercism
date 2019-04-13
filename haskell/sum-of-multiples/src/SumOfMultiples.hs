f74b40aa0ab05f1eb693bc1e2d55a6152d67921d
Patrik Iselind <patrik.mrx@gmail.com>
2019-04-13 15:05:28 +0200

    Handle zero properly

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,25 +1,15 @@
 module SumOfMultiples (sumOfMultiples) where
 
-import Data.List
+-- nub removes duplicate entries, keeping the first occurrence
+import Data.List (nub)
+
+-- concat() takes a list of lists and flattens it to a list
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum $ nub $ concat $ map (mylist (limit-1)) factors
+sumOfMultiples factors limit = sum $ nub $ concat $ map (mylist themax) filteredFactors
+    where themax = limit -1
+          filteredFactors = filter (>0) factors
 
+-- Generate a list of numbers from x to limit in steps of x
 mylist :: (Num a, Enum a) => a -> a -> [a]
 mylist limit x = [x,2*x..limit]
-
--- Produce a list of predicate functions
--- map isFactorOf [3,5]
-
--- If any of the predicate functions produce true for the remaining parameter,
--- return the remaining parameter
-
--- > :t any
--- any :: Foldable t => (a -> Bool) -> t a -> Bool
-
--- > (map isFactorOf [3,5]) <*> [1..20]
--- [False,False,True,False,False,True,False,False,True,False,False,True,False,False,True,False,False,True,False,False,False,False,False,False,True,False,False,False,False,True,False,False,False,False,True,False,False,False,False,True]
-
--- For natural numbers BELOW 20, multiples of 3 or 5
--- sum ([ x | x <- [1..19], y <- (map isFactorOf [3,5]), y x == True])
--- This gets you 93, which is 15 to much
