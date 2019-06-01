9d40b86edef454a71aae67ee069341a629efed76
Patrik Iselind <patrik.mrx@gmail.com>
2019-06-01 14:27:46 +0200

    Rename for readability

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -6,10 +6,10 @@ import Data.List (union)
 -- concat() takes a list of lists and flattens it to a list
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum $ foldr (union . mylist themax) [] filteredFactors
+sumOfMultiples factors limit = sum $ foldr (union . multiplesBelow themax) [] positiveFactors
     where themax = limit -1
-          filteredFactors = filter (>0) factors
+          positiveFactors = filter (>0) factors
 
 -- Generate a list of numbers from x to limit in steps of x
-mylist :: (Num a, Enum a) => a -> a -> [a]
-mylist limit x = [x,2*x..limit]
+multiplesBelow :: (Num a, Enum a) => a -> a -> [a]
+multiplesBelow limit x = [x,2*x..limit]
