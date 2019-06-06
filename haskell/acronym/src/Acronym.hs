77d941d395f1ba60f3dad5342220ebed3b1cd78f
Patrik Iselind <patrik.mrx@gmail.com>
2019-06-06 17:00:36 +0200

    Handle alternative word delimiters

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -2,5 +2,11 @@ module Acronym (abbreviate) where
 
 import Data.Char
 
+-- cleanCharacters will change any - characters to space characters
+cleanCharacters :: String -> String
+cleanCharacters [] = []
+cleanCharacters ('-':xs) = ' ':xs
+cleanCharacters (x:xs) = x: cleanCharacters xs
+
 abbreviate :: String -> String
-abbreviate xs = map (toUpper . head) $ words xs
+abbreviate xs = map (toUpper . head) $ words $ cleanCharacters xs
