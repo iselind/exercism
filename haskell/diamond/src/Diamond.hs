44e4f400079db64bc6a2d1fc5696eac8c7baeb8a
Patrik Iselind <patrik.mrx@gmail.com>
2019-08-06 19:27:19 +0200

    Now we can generate a proper line

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -1,20 +1,28 @@
 module Diamond (diamond) where
 
 import Data.List
+import Data.Maybe
 
-alphabet :: String
-alphabet = ['A'..'Z']
+-- replaceallbut will replace all characters in the input string with a
+-- whitespace unless the character is the needle
+replaceallbut :: Char -> String -> String
+replaceallbut needle (x:xs)
+	| x == needle = needle : replaceallbut needle xs
+	| otherwise = ' ' : replaceallbut needle xs
+replaceallbut _ [] = []
 
-charPos :: Char -> Maybe Int
-charPos x = elemIndex x alphabet
+generateline :: Char -> String
+generateline c = (reverse $ take (length-1) $ drop 1 upperAlphabet) ++ (take length upperAlphabet)
+	where length = succ $ fromJust $ elemIndex c upperAlphabet
 
-createBase :: Int -> [String]
-createBase n = replicate m (replicate m ' ')
-    where m = 2*n+1
+-- (drop 3 $ reverse $ drop 1 "abcdef") ++ (take 3 "abcedf")
+-- produce "cbabc"
+-- (reverse $ take 2 $ drop 1 "abcdef") ++ (take 2 "abcdef")
+-- produce "cbabc"
 
-diamond' :: Char -> Int -> (Maybe Int) -> Maybe String
-diamond' _ _ Nothing = Nothing
-diamond' c level pos = Just [c]
+upperAlphabet :: [Char]
+upperAlphabet = ['A' .. 'Z']
+
 
 diamond :: Char -> Maybe [String]
 diamond c = Just ( [c] : [])
