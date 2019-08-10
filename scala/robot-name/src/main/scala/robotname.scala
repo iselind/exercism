c9b5e4e38594b3da052b76784bf9526e029d7e51
Patrik Iselind <patrik.mrx@gmail.com>
2019-08-10 11:23:32 +0200

    Solve the optional challenge as well

--- scala/robot-name/src/main/scala/robotname.scala
+++ scala/robot-name/src/main/scala/robotname.scala
@@ -1,5 +1,7 @@
 object Robot {
+  val prefixes = for (x <- 'A' to 'Z'; y <- 'A' to 'Z') yield ""+x+y
   var runningNumber: Int = 0
+  val suffixSeriesLength: Int = 1000
 }
 
 class Robot() {
@@ -9,9 +11,12 @@ class Robot() {
 
   def reset() {
     name = {
-      Robot.runningNumber = Robot.runningNumber + 1
-      val runningNumber = Robot.runningNumber
-      f"AB$runningNumber%03d"
+      val prefix = Robot.prefixes(Robot.runningNumber/Robot.suffixSeriesLength)
+      val suffix = Robot.runningNumber % Robot.suffixSeriesLength
+
+      Robot.runningNumber += 1
+
+      f"$prefix%s$suffix%03d"
     }
   }
 }
