6c4b9d87c6f1b3c35829d07e0140aed0f2da0fda
Patrik Iselind <patrik.mrx@gmail.com>
2019-08-11 15:49:53 +0200

    Swap out the stack usage for a list acting like a stack

--- scala/matching-brackets/src/main/scala/matchingbrackets.scala
+++ scala/matching-brackets/src/main/scala/matchingbrackets.scala
@@ -17,14 +17,17 @@ object MatchingBrackets {
   // isPaired will return true if all the parenthesis are matched properly,
   // otherwise false will be returned
   def isPaired(data: String): Boolean = {
-    var s = Stack[Char]()
+    var s = List[Char]()
 
     try {
       for (c <- data) {
         c match {
-          case '(' | '[' | '{' => s.push(c)
+          case '(' | '[' | '{' => s = c :: s // Push c onto s
           case ')' | ']' | '}' => {
-            val cShouldMatchWith = s.pop
+            // Pop the stack
+            val cShouldMatchWith = s.head
+            s = s.tail
+
             val theChar = MatchingBrackets.matchingPairs.get(c)
             theChar match {
               case Some(b) => {
