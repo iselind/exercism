3d7145d02f2db8aa631ec4ba4a9715eba30bce1e
Patrik Iselind <patriki@axis.com>
2021-10-29 16:44:20 +0200

    Fix all tests
    Change-Id: Id5b8c0cf1e65d86890bef030c4e011920edc0468

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Text;
 
 public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        bool capitalize = false;
+        StringBuilder sb = new StringBuilder();
+        foreach (char c in identifier)
+        {
+            switch (c)
+            {
+                case ' ':
+                    sb.Append('_');
+                    break;
+                case '\0':
+                    sb.Append("CTRL");
+                    break;
+                case '-':
+                    capitalize = true;
+                    break;
+                default:
+                    if (!Char.IsLetter(c))
+                    {
+                        continue;
+                    }
+
+                    if (c >= 'α' && c <= 'ω')
+                    {
+                        continue;
+                    }
+
+                    if (capitalize)
+                    {
+                        sb.Append(Char.ToUpper(c));
+                        capitalize = false;
+                    }
+                    else
+                    {
+                        sb.Append(c);
+                    }
+                    break;
+            }
+        }
+        return sb.ToString();
     }
 }
