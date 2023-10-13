4add86ca2bb34af51ff7f9d2acfa3313f40a8327
Patrik Iselind <patrik@iselind.com>
2023-10-13 15:58:15 +0200

    solution to ellens alien game

--- cpp/ellens-alien-game/ellens_alien_game.cpp
+++ cpp/ellens-alien-game/ellens_alien_game.cpp
@@ -1,4 +1,48 @@
-namespace targets {
-// TODO: Insert the code for the alien class here
+namespace targets
+{
+    class Alien
+    {
+    public:
+        Alien(int x, int y)
+        {
+            x_coordinate = x;
+            y_coordinate = y;
+        }
+        int x_coordinate;
+        int y_coordinate;
+        int get_health()
+        {
+            return health;
+        }
+        bool hit()
+        {
+            if (health > 0)
+                health--;
 
-}  // namespace targets
+            return true; // Shield is not up
+        }
+        bool is_alive()
+        {
+            return health > 0;
+        }
+        bool teleport(int x_new, int y_new)
+        {
+            x_coordinate = x_new;
+            y_coordinate = y_new;
+            return true; // Successful teleportation
+        }
+        bool collision_detection(Alien other)
+        {
+            if (x_coordinate != other.x_coordinate)
+                return false;
+            if (y_coordinate != other.y_coordinate)
+                return false;
+
+            return true;
+        }
+
+    private:
+        int health = 3;
+    };
+
+} // namespace targets
