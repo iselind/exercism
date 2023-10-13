fd3cc353675bf55176cc211cfaee05c4f71013e4
Patrik Iselind <patrik@iselind.com>
2023-10-13 17:08:35 +0200

    Solving allergies

--- cpp/allergies/allergies.cpp
+++ cpp/allergies/allergies.cpp
@@ -1,5 +1,29 @@
 #include "allergies.h"
 
-namespace allergies {
+namespace allergies
+{
+    allergy_test::allergy_test(int score)
+    {
+        this->score = score;
+    }
+    bool allergy_test::is_allergic_to(string item)
+    {
+        if (score == 0)
+            return false;
 
-}  // namespace allergies
+        int s = items[item];
+        return (score & s) != 0;
+    }
+    unordered_set<std::string> allergy_test::get_allergies()
+    {
+        unordered_set<std::string> result{};
+
+        for (auto [key, value] : items)
+        {
+            if (is_allergic_to(key))
+                result.emplace(key);
+        }
+
+        return result;
+    }
+} // namespace allergies
--- cpp/allergies/allergies.h
+++ cpp/allergies/allergies.h
@@ -1,8 +1,33 @@
 #if !defined(ALLERGIES_H)
 #define ALLERGIES_H
 
-namespace allergies {
+#include <string>
+#include <unordered_set>
+#include <map>
 
-}  // namespace allergies
+using namespace std;
+
+namespace allergies
+{
+    class allergy_test
+    {
+    public:
+        allergy_test(int);
+        bool is_allergic_to(string);
+        unordered_set<std::string> get_allergies();
+
+    private:
+        int score;
+        map<string, int> items{
+            {"eggs", 1},
+            {"peanuts", 2},
+            {"shellfish", 4},
+            {"strawberries", 8},
+            {"tomatoes", 16},
+            {"chocolate", 32},
+            {"pollen", 64},
+            {"cats", 128}};
+    };
+} // namespace allergies
 
 #endif // ALLERGIES_H
