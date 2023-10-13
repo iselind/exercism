a678fbcf35d31277cb09999515e2573d01cfba3c
Patrik Iselind <patrik@iselind.com>
2023-10-13 14:45:44 +0200

    Solution to loglevels

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -1,5 +1,21 @@
 #include <string>
 
+using namespace std;
+
 namespace log_line
 {
+    string message(string log_line)
+    {
+        auto delimiter = log_line.find(':');
+        return log_line.substr(delimiter + 2);
+    }
+    string log_level(string log_line)
+    {
+        auto delimiter = log_line.find(']');
+        return log_line.substr(1, delimiter - 1);
+    }
+    string reformat(string log_line)
+    {
+        return message(log_line) + " (" + log_level(log_line) + ")";
+    }
 } // namespace log_line
